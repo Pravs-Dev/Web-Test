10e75c8b08ef53cd78f540048b37ea35746065de
Pravs-Dev <pravirstudy@gmail.com>
2024-09-07 16:34:37 +0200

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,17 +1,16 @@
 const request = require('supertest');
-const app = require('./app.js');
-const server = require('./server.js'); // Import the server to close later
+const { app, mongoose } = require('./app.js');  // Import mongoose along with the app
+const server = require('./server.js');  // Import the server
 
 describe('GET /', () => {
   it('should return "hello from me"', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe('Hello from me');
+    expect(res.text).toBe('hello from me');
   });
 
-  // Close the server after all tests
   afterAll(async () => {
     await mongoose.connection.close();  // Close the database connection
-    server.close(); // Close the server
+    server.close();  // Close the server
   });
 });
