13070953183af0f79d1acb4038208ff5aa195a18
Pravs-Dev <pravirstudy@gmail.com>
2024-09-07 16:24:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-import app from './app.js'
+const app = require('./app.js');
+const server = app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
 
-app.listen(3000 , () => console.log('Listening on port 3000'));
-
+module.exports = server;
