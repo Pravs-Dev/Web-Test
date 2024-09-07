55a59f07d13bb469abc10538efb0d4a97dfc847e
Pravir <pravirstudy@gmail.com>
2024-09-07 16:40:45 +0200

    Resolved merge conflicts

--- app.test.js
+++ app.test.js
@@ -1,10 +1,16 @@
 const request = require('supertest');
-const app = require('./app.js'); 
+const { app, mongoose } = require('./app.js');  // Import mongoose along with the app
+const server = require('./server.js');  // Import the server
 
 describe('GET /', () => {
   it('should return "hello from me"', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe('Hello from me');
+    expect(res.text).toBe('hello from me');
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();  // Close the database connection
+    server.close();  // Close the server
   });
 });
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-import app from './app.js'
+const { app } = require('./app.js');
 
-app.listen(3000 , () => console.log('Listening on port 3000'));
+const server = app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
 
+module.exports = server;
